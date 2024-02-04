d46a6e71b0f765379cc85313b6cc2bbb4d8d6583
Yoanna <yoanna.cao@vungle.com>
2024-02-04 17:29:25 +0800

    add landing page

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -3,16 +3,24 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <div>
-      Here is the landing page
-      <div>
-        <Button asChild>
-          <Link href="/chapters">To stories</Link>
+    <div className="flex min-h-screen flex-col justify-center gap-3 bg-pink-500 p-6 text-white">
+      <h1 className="text-3xl font-bold">Exquisite Story</h1>
+      <p>A collective storytelling journey.</p>
+      <p>
+        With Exquisite Story, you continue the story and shape how the story is
+        told paragraph by paragraph.
+      </p>
+      <p>
+        Once you tell your story, you can mint a commemorative Story Protocol
+        NFT.
+      </p>
+
+      <div className="mt-4 flex gap-4">
+        <Button asChild className="w-1/2 bg-white text-black">
+          <Link href="/graph">View IP Graph</Link>
         </Button>
-      </div>
-      <div>
-        <Button asChild>
-          <Link href="/graph">To Graph</Link>
+        <Button asChild className="w-1/2 bg-white text-black">
+          <Link href="/chapters">Continue</Link>
         </Button>
       </div>
     </div>
