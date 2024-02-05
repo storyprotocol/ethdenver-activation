b2e4446990a635d1899ed69c30da6c5d96602d04
Steven Zhang <steven@57blocks.com>
2024-02-05 23:48:32 +0800

    feat&refactor:finish get a chapter&parent content and refactor error handler

--- src/app/api/errorUtils.ts
+++ src/app/api/errorUtils.ts
@@ -1,16 +1,57 @@
 import { NextResponse } from "next/server";
 
-function HTTP400Error(code: string, msg: string): Response {
+// create MaxCountError that extend Error
+class BasicError extends Error {
+  code: string;
+  status: number;
+  constructor(code: string, status: number, message: string) {
+    super(message);
+    this.name = "MissingConfigMaxValueError";
+    this.code = code;
+    this.status = status;
+  }
+}
+
+class CusTypeError extends BasicError {
+  constructor(code: string, message: string) {
+    super(code, 400, message);
+  }
+}
+class CusEnvVarsConfigError extends BasicError {
+  constructor(code: string, message: string) {
+    super(code, 500, message);
+  }
+}
+
+class CusEntityNotFoundError extends BasicError {
+  constructor(code: string, message: string) {
+    super(code, 400, message || "Entity not found");
+  }
+}
+
+class CusEntityNotUniqueError extends BasicError {
+  constructor(code: string, message: string) {
+    super(code, 400, message || "Entity not unique");
+  }
+}
+
+class CusRangeError extends BasicError {
+  constructor(code: string, message: string) {
+    super(code, 400, message);
+  }
+}
+
+function http400Error(code: string, msg?: string): Response {
   return NextResponse.json(
     {
       code: code,
-      message: msg || "bad request",
+      message: msg || "error request",
     },
     { status: 400 },
   );
 }
 
-function HTTP500Error(code: string): Response {
+function http500Error(code: string): Response {
   return NextResponse.json(
     {
       code: code,
@@ -20,9 +61,43 @@ function HTTP500Error(code: string): Response {
   );
 }
 
-let ErrorCode = {
-  ChapterRandomQueryVarError: "C01E400-0001",
-  ChapterRandomMaxNotConfigError: "C01E500-0001",
+const ErrorCode = {
+  SystemError: "ORIGINAL_ERROR",
+  ChapterRandomQueryVarError: "Query_Var_Random",
+  ChapterRandomMaxNotConfigError: "Query_Var_Random_Not_Config",
+
+  ChapterUpDepthTypeError: "Query_Var_Depth_Type_Error",
+  ChapterUpDepthMaxError: "Query_Var_Depth_Max_Error",
+  ChapterUpDepthRangeError: "Query_Var_Depth_Range_Error",
+  ChapterIDTypeError: "Query_Var_ID_Type_Error",
+  ChapterNotExistError: "Chapter_Not_Exist",
+  ChapterIdUniqueError: "Chapter_ID_Unique_Error",
 };
 
-export { ErrorCode, HTTP400Error, HTTP500Error };
+function errorHandler(err: Error): Response {
+  if (err instanceof BasicError) {
+    const cusErr = err as BasicError;
+    switch (cusErr.status) {
+      case 400:
+        return http400Error(cusErr.code, cusErr.message);
+      case 500:
+        return http500Error(cusErr.code);
+      default:
+        return http400Error(cusErr.code);
+    }
+  } else {
+    return http400Error(ErrorCode.SystemError, err.message);
+  }
+}
+export {
+  BasicError,
+  CusEntityNotFoundError,
+  CusEntityNotUniqueError,
+  CusEnvVarsConfigError,
+  CusRangeError,
+  CusTypeError,
+  ErrorCode,
+  errorHandler,
+  http400Error,
+  http500Error,
+};
--- src/app/api/utils.ts
+++ src/app/api/utils.ts
@@ -5,6 +5,9 @@ function GetEnv(name: string) {
 let EnvKey = {
   ChapterRandomMinCount: "API_CHAPTER_RANDOM_MIN_COUNT",
   ChapterRandomMaxCount: "API_CHAPTER_RANDOM_MAX_COUNT",
+  ChapterUpWithSelfMaxDepth: "API_CHAPTER_UP_WITH_SELF_MAX_DEPTH",
+
+  ChapterRelationshipMaxlimit: "API_CHAPTER_RELATIONSHIP_MAX_LIMIT",
 };
 
 export { EnvKey, GetEnv };
