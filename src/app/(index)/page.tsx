fedae698c9ac52f1a3d7b3dc1005ea119e0501f9
yoanna-57blocks <97427646+yoanna-57blocks@users.noreply.github.com>
2024-02-05 16:58:15 +0800

    add landing page (#3)
    feat: add landing page

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,18 +1,33 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
 import Link from "next/link";
+import Image from "next/image";
+import arrowRightBlackIcon from "@/assets/common/arrow_right_black.svg";
+import { Button } from "@/components/ui/button";
 
 export default function Page() {
   return (
-    <div>
-      Here is the landing page
-      <div>
-        <Button asChild>
-          <Link href="/chapters">To stories</Link>
+    <div className="flex min-h-[calc(100vh-33px)] flex-col justify-center gap-3 p-6 text-white">
+      <h1 className="text-3xl font-bold">Exquisite Story</h1>
+      <p>A collective storytelling journey.</p>
+      <p>
+        With Exquisite Story, you continue the story and shape how the story is
+        told paragraph by paragraph.
+      </p>
+      <p>
+        Once you tell your story, you can mint a commemorative Story Protocol
+        NFT.
+      </p>
+
+      <div className="mt-4 flex gap-4">
+        <Button className={"w-1/2"}>
+          <Link href="/graph">View IP Graph</Link>
         </Button>
-      </div>
-      <div>
-        <Button asChild>
-          <Link href="/graph">To Graph</Link>
+        <Button className={"w-1/2"} asChild>
+          <Link href="/chapters">
+            <span>Continue</span>
+            <Image src={arrowRightBlackIcon} alt={"back"} className="ml-2" />
+          </Link>
         </Button>
       </div>
     </div>
