7974a2efaffec0abdce31202f2ff7661a4a517dd
ethan-57blocks <83741492+ethan-57blocks@users.noreply.github.com>
2024-02-07 16:23:48 +0800

    Delete src/app/api/cron directory

--- src/app/api/cron/uploadChapter/route.ts
+++
@@ -1,9 +0,0 @@
-export async function GET() {
-  console.log("uploading chapters...");
-  const result = await fetch(
-    `${process.env.CRON_API_ENDPOINT || "http://localhost:3000"}/api/upload/chapter`,
-  );
-  const data = await result.json();
-  console.log("chapters are uploaded.");
-  return Response.json(data);
-}
