40a00f185e9369c940d2d764d71fad66a45f3e7c
Yoanna <yoanna.cao@vungle.com>
2024-02-08 15:37:57 +0800

    jump to story protocol page

--- src/components/pages/FooterLogo.tsx
+++ src/components/pages/FooterLogo.tsx
@@ -6,12 +6,15 @@ import { usePathname } from "next/navigation";
 
 export default function FooterLogo() {
   const pathname = usePathname();
+  const toStoryprotocol = () => {
+    window.location.href = "https://www.storyprotocol.xyz";
+  };
   return (
     <>
       {pathname === "/tv" ? (
         <></>
       ) : (
-        <div className={"py-4"}>
+        <div className={"py-4"} onClick={toStoryprotocol}>
           <Image className={"mx-auto"} src={footerLogoPic} alt={"logo"} />
         </div>
       )}
