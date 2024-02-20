07c5f4bae4abe4ce8102e97b9e581e7698ac8e74
Marvin <marvin@57blocks.com>
2024-02-20 14:19:09 +0800

    feat: change chart render method from svg to canvas as svg did not support text tag

--- src/app/og/graph/[highlight_id]/route.tsx
+++ src/app/og/graph/[highlight_id]/route.tsx
@@ -3,6 +3,7 @@ import { ImageResponse } from "next/og";
 import { queryChaptersAfterID } from "@/app/api/chapters/relationship/server";
 import { generateChartData, generateChartOption } from "@/lib/chartUtils";
 import { NextRequest } from "next/server";
+import { createCanvas } from "canvas";
 
 export const dynamic = "force-dynamic";
 
@@ -16,50 +17,46 @@ export async function GET(
     };
   },
 ) {
+  const { width, height } = { width: 1200, height: 630 };
   const highlightId = params.highlight_id;
   const response = await queryChaptersAfterID({
     from_chapter_id: 0,
     limit: 1000,
   });
 
-  // In SSR mode the first container parameter is not required
-  const chart = init(null, null, {
-    renderer: "svg", // must use SVG rendering mode
-    ssr: true, // enable SSR
-    width: 1200, // need to specify height and width
-    height: 630,
-  });
+  const canvas = createCanvas(width, height);
+  const chart = init(canvas as unknown as HTMLElement);
 
   const chartData = generateChartData(response.chapters);
   const chartOptions = generateChartOption({
     chartData,
     highlightId,
+    isTv: true,
   });
   chart.setOption(chartOptions);
-  const svgStr = chart.renderToSVGString();
+
+  const dataURL = canvas.toDataURL("image/png");
   chart.dispose();
 
   return new ImageResponse(
     (
       <div
         style={{
-          backgroundColor: "white",
+          backgroundColor: "red",
           backgroundImage: "linear-gradient(168deg, #FF6E6E 0%, #9BABFF 100%)",
           height: "100%",
           width: "100%",
           display: "flex",
+          fontSize: "24px",
         }}
       >
         {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          src={`data:image/svg+xml;utf8,${encodeURIComponent(svgStr)}`}
-          alt={""}
-        />
+        <img src={dataURL} width={width} height={height} alt={""} />
       </div>
     ),
     {
-      width: 1200,
-      height: 630,
+      width: width,
+      height: height,
     },
   );
 }
