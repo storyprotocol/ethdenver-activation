c482a44d957648721c999e4bd13e335c3da50c15
DeanSui <dean@57blocks.com>
2024-02-25 12:53:19 +0800

    fix: alert animation

--- src/components/pages/SuccessAlert.tsx
+++ src/components/pages/SuccessAlert.tsx
@@ -14,6 +14,7 @@ export default function SuccessAlert({
   duration?: number;
 }) {
   const [hide, setHide] = useState(true);
+  const [isHover, setIsHover] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
@@ -22,16 +23,28 @@ export default function SuccessAlert({
   }, []);
 
   useEffect(() => {
+    let timeTag: string | number | NodeJS.Timeout | undefined;
     if (duration) {
-      setTimeout(() => {
+      timeTag = setTimeout(() => {
         setHide(true);
       }, duration + 1000);
+
+      if (isHover) {
+        clearTimeout(timeTag);
+      }
     }
-  }, [duration]);
+    return () => {
+      if (timeTag) {
+        clearTimeout(timeTag);
+      }
+    };
+  }, [duration, isHover]);
 
   return (
     <div
-      className={`shadow-[0px_2px_8px_0px_rgba(0, 0, 0, 0.10)]	opacity-1 flex h-auto rounded-2xl bg-[#C9FFDB] pb-6 pl-6 pr-4 pt-6 transition-all ${!msg || hide ? "h-0 pb-0 pl-0 pr-0 pt-0 opacity-0" : ""}`}
+      className={`shadow-[0px_2px_8px_0px_rgba(0, 0, 0, 0.10)] flex rounded-2xl bg-[#C9FFDB] transition-all ${!msg || hide ? "h-0 pb-0 pl-0 pr-0 pt-0 opacity-0" : " opacity-1 h-auto pb-6 pl-6 pr-4 pt-6"}`}
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
     >
       <div className="mr-2 grow text-base leading-normal text-[#282828]">
         <div>{title}</div>
