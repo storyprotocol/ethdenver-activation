f58463b80aa1746860a5b32ae7eb17ef3bff5f1c
DeanSui <dean@57blocks.com>
2024-02-26 11:00:50 +0800

    fix: Radix-ui Toast

--- src/components/pages/SuccessAlert.tsx
+++ src/components/pages/SuccessAlert.tsx
@@ -2,7 +2,9 @@
 
 import Image from "next/image";
 import shareSuccessIconPic from "@/assets/chapter/shareSuccess_icon.svg";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import * as ToastPrimitive from "@radix-ui/react-toast";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function SuccessAlert({
   title,
@@ -13,50 +15,60 @@ export default function SuccessAlert({
   msg?: string;
   duration?: number;
 }) {
-  const [hide, setHide] = useState(true);
-  const [isHover, setIsHover] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setHide(false);
-    }, 1000);
-  }, []);
-
-  useEffect(() => {
-    let timeTag: string | number | NodeJS.Timeout | undefined;
-    if (duration) {
-      timeTag = setTimeout(() => {
-        setHide(true);
-      }, duration + 1000);
-
-      if (isHover) {
-        clearTimeout(timeTag);
-      }
-    }
-    return () => {
-      if (timeTag) {
-        clearTimeout(timeTag);
-      }
-    };
-  }, [duration, isHover]);
+  const [toastList, setToastList] = useState(["success"]);
 
   return (
-    <div
-      className={`shadow-[0px_2px_8px_0px_rgba(0, 0, 0, 0.10)] flex rounded-2xl bg-[#C9FFDB] transition-all ${!msg || hide ? "h-0 pb-0 pl-0 pr-0 pt-0 opacity-0" : " opacity-1 h-auto pb-6 pl-6 pr-4 pt-6"}`}
-      onMouseEnter={() => setIsHover(true)}
-      onMouseLeave={() => setIsHover(false)}
-    >
-      <div className="mr-2 grow text-base leading-normal text-[#282828]">
-        <div>{title}</div>
-        <div>{msg}</div>
-      </div>
-      <div className="flex content-center	justify-center">
-        <Image
-          className="ml-2.5"
-          src={shareSuccessIconPic}
-          alt={"Success Icon"}
-        />
-      </div>
-    </div>
+    <ToastPrimitive.Provider>
+      <AnimatePresence>
+        {toastList.map((item) => (
+          <ToastPrimitive.Root
+            duration={duration}
+            onOpenChange={(open) => {
+              if (!open) setToastList([]);
+            }}
+            asChild
+            forceMount
+            key={item}
+          >
+            <motion.li
+              initial={{
+                y: -100,
+                scale: 0.6,
+                opacity: 0,
+              }}
+              animate={{
+                y: 0,
+                scale: 1,
+                opacity: 1,
+                transition: { duration: 0.3 },
+              }}
+              exit={{
+                scale: 0.9,
+                opacity: 0,
+                transition: { duration: 0.15 },
+              }}
+              layout
+            >
+              <div
+                className={`shadow-[0px_2px_8px_0px_rgba(0, 0, 0, 0.10)] flex rounded-2xl bg-[#C9FFDB] pb-6 pl-6 pr-4 pt-6`}
+              >
+                <div className="mr-2 grow text-base leading-normal text-[#282828]">
+                  <div>{title}</div>
+                  <div>{msg}</div>
+                </div>
+                <div className="flex content-center	justify-center">
+                  <Image
+                    className="ml-2.5"
+                    src={shareSuccessIconPic}
+                    alt={"Success Icon"}
+                  />
+                </div>
+              </div>
+            </motion.li>
+          </ToastPrimitive.Root>
+        ))}
+      </AnimatePresence>
+      <ToastPrimitive.Viewport />
+    </ToastPrimitive.Provider>
   );
 }
