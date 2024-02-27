472f870987cfb45e235665061f0b65515348b560
Steven Zhang <steven@57blocks.com>
2024-02-27 15:37:38 +0800

    feat:return 1 left chapter and others random chapters

--- src/interface/chapter.ts
+++ src/interface/chapter.ts
@@ -1,3 +1,6 @@
+import { ChapterMO } from "@/app/api/model";
+import exp from "constants";
+
 export interface Chapter {
   id: number;
   content: string;
@@ -6,4 +9,15 @@ export interface Chapter {
   wallet_address: string;
   path: number[];
   child_count: number;
+}
+
+export function toChapter(chapter: ChapterMO): Chapter {
+  return {
+    id: chapter.id,
+    content: chapter.content,
+    story_id: chapter.story_id,
+    parent_id: chapter.parent_id,
+    wallet_address: chapter.wallet_address,
+    path: chapter.path,
+  } as Chapter;
 }
